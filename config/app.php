45ad2307ae02eae51e22d2fdacd34760dca4ed89
paulkrokhmal <paul.krokhmal@gmail.com>
2021-11-16 18:00:55 +0200

    socialite configuration

--- config/app.php
+++ config/app.php
@@ -175,6 +175,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Laravel\Socialite\SocialiteServiceProvider::class
     ],
 
     /*
@@ -229,7 +230,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Socialite' => Laravel\Socialite\Facades\Socialite::class,
+        'OpenWeatherService' => \App\Facades\OpenWeatherFacade::class,
+        'IpstackService' => \App\Facades\IpstackFacade::class
     ],
 
 ];
