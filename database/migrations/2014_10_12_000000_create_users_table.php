62624fc3807a859a3e7f786f28ec2974b9640a3a
paulkrokhmal <paul.krokhmal@gmail.com>
2021-11-16 17:54:27 +0200

    update user table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,12 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->enum('status', ['Active', 'Inactive'])->default('Active');
+            $table->string('social_id')->nullable();
+            $table->enum('social_type', ['Google'])->default('Google'); // Can be extended in future
+            $table->string('profile')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
